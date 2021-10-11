db560e4305cf530945c65ef3a53bbcfae10d0d77
Alisson Youssf <youssfbr@gmail.com>
2021-10-11 14:56:49 -0300

    Context Migrations

---
+++ BookStore/Context/AppDataContext.cs
@@ -0,0 +1,14 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Context
+{
+    public class AppDataContext : DbContext
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options) :  base(options) { }
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+    }
+}
+ 
