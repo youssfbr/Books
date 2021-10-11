659bd7942f0b6c9b2c8a15414c419f72f6509ea7
Alisson Youssf <youssfbr@gmail.com>
2021-10-11 11:34:43 -0300

    Project created

---
+++ BookStore/Models/Autor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Autor
+    {        
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Campo inválido")]
+        public string Nome { get; set; }
+
+        public ICollection<Livro> Livros { get; set; }
+    }
+}
---
+++ BookStore/Models/Livro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Livro
+    {        
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Campo inválido")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Campo inválido")]
+        public string ISBN { get; set; }
+
+        [Required(ErrorMessage = "*")]
+        [DataType(DataType.Date)]
+        public DateTime DataLancamento { get; set; }
+
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+        public ICollection<Autor> Autores { get; set; }
+    }
+}
